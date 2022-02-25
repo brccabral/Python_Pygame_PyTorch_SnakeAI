f3b49631aaaf7c604da2a43243dad923a5d43d52
Bruno Cabral <cabral.brc@gmail.com>
2022-02-24 20:34:48 -0800

    training loop

--- agent.py
+++ agent.py
@@ -22,13 +22,15 @@ class Agent:
     def get_state(self, game: SnakeGameAI):
         pass
 
-    def get_move(self, state, action, reward, next_state, game_over):
+    def remember(self, state, action, reward, next_state, game_over):
         pass
 
     def train_long_memory(self):
+        # trains in all the previous moves
+        # increasing agent performance
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, game_over):
         pass
 
     def get_action(self, state):
@@ -36,7 +38,40 @@ class Agent:
 
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    best_score = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        action = agent.get_action(state_old)
+
+        # perform action, play game, and get new state
+        reward, game_over, score = game.play_step(action)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, action, reward, state_new, game_over)
+
+        # remember
+        agent.remember(state_old, action, reward, state_new, game_over)
+
+        if game_over:
+            game.reset()
+            agent.number_of_games += 1
+            if score > best_score:
+                best_score = score
+            print(f'Game {agent.number_of_games} Score {score} Record {best_score}')
+
+            # train long memory (also called replay memory, or experience replay)
+            agent.train_long_memory()
+
+
 
 
 if __name__ == "__main__":
