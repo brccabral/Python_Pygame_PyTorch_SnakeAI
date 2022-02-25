bdb55d7424b7e746492c290aaedae2de10d939ec
Bruno Cabral <cabral.brc@gmail.com>
2022-02-24 21:22:20 -0800

    QTrainer init

--- model.py
+++ model.py
@@ -22,3 +22,13 @@ class Linear_QNet(nn.Module):
 
         file_name = os.path.join(model_folder_path, file_name)
         torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model: Linear_QNet, lr, gamma):
+        self.model = model
+        self.lr = lr
+        self.gamma = gamma
+
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+    
