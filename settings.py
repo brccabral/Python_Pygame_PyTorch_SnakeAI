ad8172b0b044876b9e8a4486777da854be9741f0
Bruno Cabral <cabral.brc@gmail.com>
2022-03-03 11:14:55 -0800

    new settings

--- settings.py
+++ settings.py
@@ -6,6 +6,8 @@ from enum import Enum
 BLOCK_SIZE = 20
 BLOCK_DRAW_OFFSET = int(BLOCK_SIZE*0.2)
 BLOCK_SIZE_OFFSET = int(BLOCK_SIZE*0.6)
+GAME_TABLE_COLUMNS = 40
+GAME_TABLE_ROWS = 24
 
 WHITE = (255, 255, 255)
 RED = (200, 0, 0)
@@ -16,12 +18,12 @@ GREEN2 = (0, 255, 100)
 BLACK = (0, 0, 0)
 
 # game display
-GAME_WIDTH = 800
-GAME_HEIGHT = 480
+GAME_WIDTH = GAME_TABLE_COLUMNS * BLOCK_SIZE
+GAME_HEIGHT = GAME_TABLE_ROWS * BLOCK_SIZE
 GAME_DISPLAY_PADDING = 2
 
 # main display
-DISPLAY_GUI = False
+DISPLAY_GUI = True
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 480
 CLOCK_SPEED = 120
@@ -30,6 +32,8 @@ CLOCK_SPEED = 120
 MAX_GENERATIONS = 100_000
 NUMBER_OF_AGENTS = 30
 MUTATION_PROBABILITY = 0.01
+MUTATION_RATE = 0.001
+FITNESS_TARGET = pow(GAME_TABLE_COLUMNS * GAME_TABLE_ROWS, 2)
 
 
 class Play_Type(Enum):
@@ -44,6 +48,6 @@ PLAY_TYPE = Play_Type.AI
 MAX_MEMORY = 100_000  # how many previous moves will be stored in memory_deque
 BATCH_SIZE = 1000
 LR = 0.001
-INPUT_SIZE = 10  # has to be the length of Agent.get_state
+INPUT_SIZE = 31  # has to be the length of Agent.get_state
 HIDDEN_SIZE = 256
 OUTPUT_SIZE = 4  # has to be the number of possible actions, Agent.get_action
