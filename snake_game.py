9ace4f9f075a0e533e556488e18ba94ad230e2aa
Bruno Cabral <cabral.brc@gmail.com>
2022-03-20 09:55:12 -0700

    removed unused functions and classes

--- snake_game.py
+++ snake_game.py
@@ -1,9 +1,9 @@
 import datetime
 import math
-from typing import List, Tuple
+from typing import Tuple
 import pygame
 import random
-from settings import GAME_WIDTH, GAME_HEIGHT, GAME_TABLE_ROWS, GAME_TABLE_COLUMNS, GREEN1, GREEN2, BLACK, BLUE1, BLUE2, BLOCK_SIZE, BLOCK_DRAW_OFFSET, BLOCK_SIZE_OFFSET, WHITE, RED
+from settings import GAME_WIDTH, GAME_HEIGHT, GAME_TABLE_ROWS, GAME_TABLE_COLUMNS, GREEN1, GREEN2, BLACK, BLUE1, BLUE2, BLOCK_SIZE, BLOCK_DRAW_OFFSET, WHITE, RED
 from collections import deque
 
 
@@ -128,95 +128,6 @@ class Direction:
             return current_direction
 
 
-class Node:
-    def __init__(self, point: Point, previous: "Node", from_direction: Direction, game: "SnakeGameAI"):
-        self.previous = previous
-        self.point = point
-        self.cost = 0
-        self.next_right = None
-        self.next_left = None
-        self.next_down = None
-        self.next_up = None
-
-        if from_direction == Direction.RIGHT:
-            self.next_left = previous
-        elif from_direction == Direction.LEFT:
-            self.next_right = previous
-        elif from_direction == Direction.UP:
-            self.next_down = previous
-        elif from_direction == Direction.DOWN:
-            self.next_up = previous
-
-        if self.point.x % 2:
-            self.next_up = -1
-        else:
-            self.next_down = -1
-
-        if self.point.y % 2:
-            self.next_right = -1
-        else:
-            self.next_left = -1
-
-        if previous is not None:
-            self.update_cost(game)
-
-        if game.is_out_of_board(self.point+Direction.RIGHT):
-            self.next_right = -1
-        if game.is_out_of_board(self.point+Direction.LEFT):
-            self.next_left = -1
-        if game.is_out_of_board(self.point+Direction.DOWN):
-            self.next_down = -1
-        if game.is_out_of_board(self.point+Direction.UP):
-            self.next_up = -1
-
-        if game.step_cost(self.point + Direction.RIGHT, self.cost) < 0:
-            self.next_right = -1
-        if game.step_cost(self.point + Direction.LEFT, self.cost) < 0:
-            self.next_left = -1
-        if game.step_cost(self.point + Direction.DOWN, self.cost) < 0:
-            self.next_down = -1
-        if game.step_cost(self.point + Direction.UP, self.cost) < 0:
-            self.next_up = -1
-
-    def is_complete(self):
-        return (self.next_right is not None and self.next_left is not None
-                and self.next_down is not None and self.next_up is not None)
-
-    def get_next_side(self, food: Point):
-        next_side = None
-        food_direction = (food - self.point).unit()
-        if self.next_right is None and food_direction.x > 0:
-            next_side = Direction.RIGHT
-        elif self.next_left is None and food_direction.x < 0:
-            next_side = Direction.LEFT
-        elif self.next_down is None and food_direction.y > 0:
-            next_side = Direction.DOWN
-        elif self.next_up is None and food_direction.y < 0:
-            next_side = Direction.UP
-        if next_side is not None:
-            return next_side
-
-        if self.next_right is None:
-            next_side = Direction.RIGHT
-        elif self.next_left is None:
-            next_side = Direction.LEFT
-        elif self.next_down is None:
-            next_side = Direction.DOWN
-        elif self.next_up is None:
-            next_side = Direction.UP
-        return next_side
-
-    def update_cost(self, game: "SnakeGameAI"):
-        if self.previous is not None:
-            next_cost = self.previous.cost + 1
-        else:
-            next_cost = 0
-        self.cost = game.step_cost(self.point, next_cost)
-
-    def __repr__(self):
-        return f"Node({self.point}, cost:{self.cost}, previous:{self.previous.point if self.previous is not None else -1})"
-
-
 class SnakeGameAI:
 
     def __init__(self):
@@ -308,18 +219,6 @@ class SnakeGameAI:
             return True
         return False
 
-    def step_cost(self, pt: Point, cost: int):
-        if self.is_out_of_board(pt):
-            return -1
-        if pt in self.snake:
-            snake_index = self.snake.index(pt)
-            if snake_index + cost > len(self.snake):
-                return cost
-            return -1
-        if pt == self.food:
-            cost += 1
-        return cost
-
     def _get_distances(self):
         self.manhattan_distances = [
             (self.head + Direction.DOWN).distance(self.food),
@@ -327,124 +226,6 @@ class SnakeGameAI:
             (self.head + Direction.RIGHT).distance(self.food),
             (self.head + Direction.UP).distance(self.food)
         ]
-
-    def traverse_table(self):
-        head = self.snake[0]
-        head_node = Node(head, None, None, self)
-        head_node.cost = 0  # reset cost for head
-        self.costs = [-1, -1, -1, -1]
-
-        while not head_node.is_complete():
-            steps = 0
-            found_food = False
-            # reset table
-            self.traverse_path: List[List[Node]] = [
-                [0 for _ in range(GAME_TABLE_COLUMNS)] for __ in range(GAME_TABLE_ROWS)]
-            self.traverse_path[head.y][head.x] = head_node
-
-            # get next side
-            current_side = head_node.get_next_side(
-                self.food)  # safe because head is not complete
-            current_node = Node(head + current_side,
-                                head_node, current_side, self)
-            if current_side == Direction.DOWN:
-                head_node.next_down = current_node
-            elif current_side == Direction.LEFT:
-                head_node.next_left = current_node
-            elif current_side == Direction.RIGHT:
-                head_node.next_right = current_node
-            elif current_side == Direction.UP:
-                head_node.next_up = current_node
-
-            node = current_node
-
-            while True:
-                self._display_block(WHITE, node.point)
-                steps += 1
-
-                self.traverse_path[node.point.y][node.point.x] = node
-                text = self.font_symbols.render(
-                    self._get_unicode(node.point), True, BLACK)
-                self.display.blit(
-                    text, [node.point.x*BLOCK_SIZE, node.point.y*BLOCK_SIZE])
-                # print("*" * GAME_TABLE_COLUMNS)
-                # print(self.get_board())
-
-                if node.point == self.food:
-                    steps += -1
-                    found_food = True
-                if steps >= len(self.snake) and found_food:
-                    break
-
-                next_side = node.get_next_side(self.food)
-                if next_side is None:
-                    node = node.previous
-                    steps += -1
-                    if node is None:
-                        break
-                    if node.point == current_node.point:
-                        break
-                    if node.point == head:
-                        break
-                else:
-                    next_point = node.point + next_side
-                    if self.traverse_path[next_point.y][next_point.x] != 0:
-                        new_node = self.traverse_path[next_point.y][next_point.x]
-                    else:
-                        new_node = Node(node.point + next_side,
-                                        node, next_side, self)
-                    if next_side == Direction.DOWN:
-                        node.next_down = new_node
-                    elif next_side == Direction.LEFT:
-                        node.next_left = new_node
-                    elif next_side == Direction.RIGHT:
-                        node.next_right = new_node
-                    elif next_side == Direction.UP:
-                        node.next_up = new_node
-                    node = new_node
-
-            if current_side == Direction.DOWN:
-                self.costs[0] = len(self.snake)/steps if steps > 0 else -1
-            elif current_side == Direction.LEFT:
-                self.costs[1] = len(self.snake)/steps if steps > 0 else -1
-            elif current_side == Direction.RIGHT:
-                self.costs[2] = len(self.snake)/steps if steps > 0 else -1
-            elif current_side == Direction.UP:
-                self.costs[3] = len(self.snake)/steps if steps > 0 else -1
-
-    def towards_food(self):
-        self.towards_food_direction = [
-            self.is_towards_food(self.head, Direction.DOWN),
-            self.is_towards_food(self.head, Direction.LEFT),
-            self.is_towards_food(self.head, Direction.RIGHT),
-            self.is_towards_food(self.head, Direction.UP),
-        ]
-
-    def is_towards_food(self, point: Point, direction: Direction):
-        point_allowed_directions = point.allowed_directions()  # (1,1) / (-1,-1)
-        if direction == Direction.DOWN and point_allowed_directions.y != 1:
-            return -1
-        if direction == Direction.LEFT and point_allowed_directions.x != -1:
-            return -1
-        if direction == Direction.RIGHT and point_allowed_directions.x != 1:
-            return -1
-        if direction == Direction.UP and point_allowed_directions.y != -1:
-            return -1
-
-        next_point: Point = point + direction
-        if self.is_out_of_board(next_point):
-            return -1
-        if self.food == next_point:
-            return 1
-
-        food_direction: Point = next_point.target_directions(
-            self.food)  # (0,1) / (1,0)
-        next_allowed_directions = next_point.allowed_directions()  # (1,1) / (-1,-1)
-
-        if food_direction.x == next_allowed_directions.x or food_direction.y == next_allowed_directions.y:
-            return 1
-
-        return -1
 
     def update_ui(self):
         self.display.fill(BLACK)
@@ -460,7 +241,6 @@ class SnakeGameAI:
         text = self.font.render(
             "Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
-        # pygame.display.update()
 
     def _display_block(self, color: Tuple[int, int, int], point: Point, offset: int = 0):
         pygame.draw.rect(self.display, color, pygame.Rect(
