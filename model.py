19db744396cfba027338e34354aa7adfd4ebd9d2
Bruno Cabral <cabral.brc@gmail.com>
2025-03-25 12:32:15 -0700

    prediction optimization

--- model.py
+++ model.py
@@ -43,31 +43,30 @@ class QTrainer:
 
     def train_step(
         self,
-        state_old_: tuple[npt.NDArray[np.int_]],
-        action_: tuple[list[int]],
-        reward_: tuple[int],
-        state_new_: tuple[npt.NDArray[np.int_]],
-        done_: tuple[bool],
+        state_old_: tuple[npt.NDArray[np.int_], ...],
+        action_: tuple[list[int], ...],
+        reward_: tuple[int, ...],
+        state_new_: tuple[npt.NDArray[np.int_], ...],
+        done_: tuple[bool, ...],
     ):
         state_old = torch.tensor(np.array(state_old_), dtype=torch.float, device=self.device)
         state_new = torch.tensor(np.array(state_new_), dtype=torch.float, device=self.device)
         action = torch.tensor(np.array(action_), dtype=torch.long, device=self.device)
-        reward = torch.tensor(np.array(reward_), dtype=torch.float, device=self.device)
-        done = done_
 
         # 1: predict Q values with current state
         pred_action: torch.Tensor = self.model(state_old)  # list
 
         # 2: Q_new = r + y * max(next_predicted_Q_value) -> only do this if not done
-        target = pred_action.clone()
-        for index in range(len(done)):
-            Q_new = reward[index]
-            if not done[index]:
-                Q_new = reward[index] + self.gamma * torch.max(self.model(state_new))
+        target = pred_action.detach().clone()
+        with torch.no_grad():
+            for index, done in enumerate(done_):
+                Q_new = reward_[index]
+                if not done:
+                    Q_new = Q_new + self.gamma * torch.max(self.model(state_new[index])).item()
 
-            target[index][int(torch.argmax(action).item())] = Q_new
+                target[index][int(torch.argmax(action[index]).item())] = Q_new
 
         self.optimizer.zero_grad()
-        loss: torch.Tensor = self.criterion(target, pred_action)
+        loss: torch.Tensor = self.criterion(pred_action, target)
         loss.backward()
         self.optimizer.step()
